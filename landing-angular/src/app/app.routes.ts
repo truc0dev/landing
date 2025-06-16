18d6e88d9793b4f2954ecb4c17e20b3ccd981909
Juan Machado <juanmchado1@gmail.com>
2025-06-15 23:46:23 -0300

    deployar en vercel

--- landing-angular/src/app/app.routes.ts
+++ landing-angular/src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '@angular/router';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
-  {
-    path: '404',
-    component: NotFoundComponent
-  },
   {
     path: '**',
     redirectTo: '404'
